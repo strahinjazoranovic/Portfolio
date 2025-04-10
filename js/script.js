da2eec827aaf4fad777493a27886e6e961576ae8
strahinjazoranovic <6042877@mborijnland.nl>
2025-04-10 13:57:43 +0200

    V1.48
    1. Contact form
    
    2. Andere kleine veranderingen

--- js/script.js
+++ js/script.js
@@ -12,3 +12,29 @@ const observer = new IntersectionObserver((entries, observer) => {
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
 
+
+const contactForm = document.getElementById("contact-form");
+const overlay = document.getElementById("overlay");
+const contactButton = document.querySelector(".contact");
+const contactClose = document.getElementById("close-contact");
+
+// Laat de form en overlay zien
+contactButton.addEventListener("click", () => {
+  contactForm.style.display = "block";
+  overlay.classList.add("active");
+  document.body.style.overflow = "hidden";
+});
+
+// Sluit de form en overlay als je op de sluit knop klikt
+contactClose.addEventListener("click", () => {
+  contactForm.style.display = "none";
+  overlay.classList.remove("active");
+  document.body.style.overflow = "";
+});
+
+// Sluit de form en overlay als je op de overlay klikt
+overlay.addEventListener("click", () => {
+  contactForm.style.display = "none";
+  overlay.classList.remove("active");
+  document.body.style.overflow = "";
+})
