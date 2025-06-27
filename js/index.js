da2254114b27a19872bbe7ce415e84f2591c3221
strahinjazoranovic <6042877@mborijnland.nl>
2025-06-27 16:07:56 +0200

    V1.00
    Introduce the main HTML, CSS, and JavaScript files for the portfolio website, including responsive design, animations, and a contact form. Add all required images and SVG icons for skills, projects, and social links.

---
+++ js/index.js
@@ -0,0 +1,40 @@
+// Animation handeler for the hidden elements once you scroll
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+const contactForm = document.getElementById("contact-form");
+const overlay = document.getElementById("overlay");
+const contactButton = document.querySelector(".contact");
+const contactClose = document.getElementById("close-contact");
+
+// Laat de form en overlay zien
+contactButton.addEventListener("click", () => {
+  contactForm.style.display = "block";
+  overlay.classList.add("active");
+  document.body.style.overflow = "hidden";
+});
+
+// Sluit de form en overlay als je op de sluit knop klikt
+contactClose.addEventListener("click", () => {
+  contactForm.style.display = "none";
+  overlay.classList.remove("active");
+  document.body.style.overflow = "";
+});
+
+// Sluit de form en overlay als je op de overlay klikt
+overlay.addEventListener("click", () => {
+  contactForm.style.display = "none";
+  overlay.classList.remove("active");
+  document.body.style.overflow = "";
+})
